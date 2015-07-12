7da353b36bab64b72e44bf9f8dc0752a59b93cb8
FGtatsuro <fujiistorage@gmail.com>
2015-07-13 08:51:52 +0900

    Schema model.

--- caprice/models.py
+++ caprice/models.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Dummy(object):
+from sqlalchemy import Column, String
 
-    def list(self):
-        return [{'title': 'title1'}, {'title': 'title2'}]
+from .db import Base
+
+__all__ = ['Schema']
+
+class Schema(Base):
+
+    __tablename__ = 'schemas'
+
+    # TODO: allow Only UUID? or user defined ID too?
+    id = Column(String, primary_key=True)
+    # TODO: JSON uniqueness is needed
+    body = Column(String)
+
+    # ID is generated in Python context(=in application)
+    def __init__(self, id=None, body=None):
+        this.id = id
+        this.body = body
+
+    def __repr__(self):
+        return '<Schema: {!r}>'.format(self.body)
