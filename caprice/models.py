75858502e0b502900dd9687946d58ab3173a8e42
FGtatsuro <fujiistorage@gmail.com>
2015-07-14 08:49:22 +0900

    Capsulate the process to save schema in database.

--- caprice/models.py
+++ caprice/models.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import Column, String
 
-from .db import Base
+from .db import Base, Session
 
 __all__ = ['Schema']
 
@@ -18,8 +18,22 @@ class Schema(Base):
 
     # ID is generated in Python context(=in application)
     def __init__(self, id=None, body=None):
-        this.id = id
-        this.body = body
+        self.id = id
+        self.body = body
 
     def __repr__(self):
         return '<Schema: {!r}>'.format(self.body)
+
+    # TODO: Use contextmanager. Ref. http://docs.sqlalchemy.org/en/rel_1_0/orm/session_basics.html
+    def save(self):
+        s = Session()
+        s.add(self)
+        try:
+            # TODO: logger
+            s.commit()
+        except:
+            s.rollback()
+            raise RuntimeError('')
+        finally:
+            # TODO: logger
+            s.close()
